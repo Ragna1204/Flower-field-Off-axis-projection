3bd6646d41672515b47605a221ce3613b609e043
Ragna1204 <samarth.dun@gmail.com>
2026-01-27 03:50:13 +0530

    time synced texts with afterthought

--- message_renderer.py
+++ message_renderer.py
@@ -41,70 +41,70 @@ class MessageRenderer:
         """
         messages = [
             # During Afterthought - minimal text
-            {"text": "Sorry for taking this long", "start": 7, "duration": 4, "pause": 0},
-            {"text": "Didnt want to rush this", "start": 12, "duration": 4, "pause": 0},
-            {"text": "This was the \"project\" I \nhad to do lol", "start": 22, "duration": 5, "pause": 0},
-            {"text": "Lowkey took way more time than I expected", "start": 30, "duration": 5, "pause": 0},
-            {"text": "Sorry if this is a bad time", "start": 35, "duration": 4, "pause": 0},
-            {"text": "Tabiyat kharab 😞", "start": 41, "duration": 4, "pause": 0},
-            {"text": "Sorry if I did or said something I shouldnt have", "start": 45, "duration": 5, "pause": 3},
+            {"text": "Sorry it took so long", "start": 7, "duration": 3.5, "pause": 0},
+            {"text": "Didnt want to rush this", "start": 12, "duration": 3.5, "pause": 0},
+            {"text": "This was the \"project\" I \nhad to do lol", "start": 26, "duration": 4, "pause": 0},
+            {"text": "Lowkey took way more time \nthan I expected", "start": 32, "duration": 5, "pause": 0},
+            {"text": "Hope its not a bad time", "start": 72, "duration": 4, "pause": 0},
+            {"text": "Sorry if I did or said \nsomething I shouldnt have", "start": 78, "duration": 5, "pause": 3},
+            {"text": "Hope you dont have the fever \nanymore", "start": 85, "duration": 4, "pause": 0},
             
-            {"text": "Yk that sunflowers look at each other\nwhen not in sunlight?", "start": 51, "duration": 6, "pause": 0},
-            {"text": "Anyways", "start": 59, "duration": 3, "pause": 0},
-            {"text": "I dedicate this song to you", "start": 64, "duration": 5, "pause": 0},
-            {"text": "Band karde kuch ni hai iske baad", "start": 71, "duration": 4, "pause": 3},
+            {"text": "Yk that sunflowers look at each other\nwhen not in sunlight?", "start": 108, "duration": 5, "pause": 0},
+            {"text": "Anyways", "start": 115, "duration": 1, "pause": 0},
+            {"text": "This would be the song \nid dedicate to u", "start": 148, "duration": 4, "pause": 0},
+            {"text": "Band karde kuch ni hai \niske baad", "start": 160, "duration": 3, "pause": 3},
             
-            {"text": "Nahi?", "start": 165, "duration": 2, "pause": 0},
-            {"text": "Jaisi marzi", "start": 170, "duration": 3, "pause": 0},
+            {"text": "Nahi?", "start": 168, "duration": 2, "pause": 0},
+            {"text": "Jaisi marzi", "start": 176, "duration": 2, "pause": 0},
             {"text": "If you are still here...", "start": 185, "duration": 4, "pause": 0},
             
             # Touch Tank starts ~95-100s (after Afterthought 3:14 = 194s)
             # Adjust these based on actual song length
-            {"text": ":)", "start": 165, "duration": 3, "pause": 0},
-            {"text": "None of ts is done w ai btw", "start": 170, "duration": 4, "pause": 0},
-            {"text": "Okay omveer appreciation day", "start": 176, "duration": 4, "pause": 0},
-            {"text": "W music taste btw", "start": 182, "duration": 4, "pause": 0},  # when beat starts
-            {"text": "What else", "start": 188, "duration": 3, "pause": 0},
-            {"text": "Idk nothing much\n<(￣︶￣)>", "start": 193, "duration": 4, "pause": 0},
+            {"text": ":)", "start": 191, "duration": 3, "pause": 0},
+            {"text": "None of ts is done w ai btw", "start": 195, "duration": 4, "pause": 0},
+            {"text": "Okay omveer appreciation day", "start": 201, "duration": 4, "pause": 0},
+            {"text": "W music taste btw", "start": 207, "duration": 4, "pause": 0},  # when beat starts
+            {"text": "What else", "start": 213, "duration": 3, "pause": 0},
+            {"text": "Idk nothing much\n<(￣︶￣)>", "start": 218, "duration": 4, "pause": 0},
             
-            {"text": "I have fun whenever im with you\nGenuinely", "start": 199, "duration": 5, "pause": 0},
-            {"text": "I like being with you", "start": 206, "duration": 4, "pause": 0},
-            {"text": "We both have our differences", "start": 212, "duration": 4, "pause": 0},
-            {"text": "I know a lot of people\nbut i dont hang out with a lot of people", "start": 218, "duration": 6, "pause": 0},
-            {"text": "You are amongst the very few\nI like being with", "start": 226, "duration": 5, "pause": 0},
+            {"text": "I have fun whenever im with you\nGenuinely", "start": 225, "duration": 5, "pause": 0},
+            {"text": "I like being with you", "start": 232, "duration": 4, "pause": 0},
+            {"text": "We both have our differences", "start": 238, "duration": 4, "pause": 0},
+            {"text": "I know a lot of people\nbut i dont hang out with a lot of people", "start": 244, "duration": 6, "pause": 0},
+            {"text": "You are amongst the very few\nI like being with", "start": 252, "duration": 5, "pause": 0},
             
-            {"text": "We also got the lifetime bestie scar too, twin", "start": 233, "duration": 5, "pause": 0},
-            {"text": "Certified trauma bonding", "start": 240, "duration": 4, "pause": 0},
-            {"text": "Even then, the only thing in my head was\nki tujhe kuch nahi hona chahiye", "start": 246, "duration": 6, "pause": 0},
-            {"text": "Still got the scar", "start": 254, "duration": 3, "pause": 0},
-            {"text": "Looks cool not even kidding", "start": 259, "duration": 4, "pause": 0},
-            {"text": "Mine not yours", "start": 265, "duration": 3, "pause": 0},
-            {"text": "Yours too 👁️👅👁️", "start": 270, "duration": 3, "pause": 0},
+            {"text": "We also got the lifetime bestie scar too, twin", "start": 259, "duration": 5, "pause": 0},
+            {"text": "Certified trauma bonding", "start": 266, "duration": 4, "pause": 0},
+            {"text": "Even then, the only thing in my head was\nki tujhe kuch nahi hona chahiye", "start": 272, "duration": 6, "pause": 0},
+            {"text": "Still got the scar", "start": 278, "duration": 3, "pause": 0},
+            {"text": "Looks cool not even kidding", "start": 283, "duration": 4, "pause": 0},
+            {"text": "Mine not yours", "start": 289, "duration": 3, "pause": 0},
+            {"text": "Yours too 👁️👅👁️", "start": 294, "duration": 3, "pause": 0},
             
-            {"text": "Should not be typing this much", "start": 275, "duration": 4, "pause": 0},
-            {"text": "If you wish to disable this text,\ngo settings > hide text", "start": 281, "duration": 5, "pause": 3},
-            {"text": "Did not add that shit", "start": 291, "duration": 3, "pause": 0},
+            {"text": "Should not be typing this much", "start": 299, "duration": 4, "pause": 0},
+            {"text": "If you wish to disable this text,\ngo settings > hide text", "start": 305, "duration": 5, "pause": 3},
+            {"text": "Did not add that shit", "start": 311, "duration": 3, "pause": 0},
             
-            {"text": "I want to know you more", "start": 296, "duration": 4, "pause": 0},
-            {"text": "I want you to know me more too", "start": 302, "duration": 4, "pause": 0},
-            {"text": "We leave a lot of planned things undone", "start": 308, "duration": 5, "pause": 0},
-            {"text": "There are many things id want to do together", "start": 315, "duration": 5, "pause": 0},
-            {"text": "Chowki dhani definately being one of them 🥀", "start": 322, "duration": 5, "pause": 0},
-            {"text": "Hmm split fiction", "start": 329, "duration": 3, "pause": 0},
+            {"text": "I want to know you more", "start": 316, "duration": 4, "pause": 0},
+            {"text": "I want you to know me more too", "start": 322, "duration": 4, "pause": 0},
+            {"text": "We leave a lot of planned things undone", "start": 328, "duration": 5, "pause": 0},
+            {"text": "There are many things id want to do together", "start": 334, "duration": 5, "pause": 0},
+            {"text": "Chowki dhani definately being one of them 🥀", "start": 341, "duration": 5, "pause": 0},
+            {"text": "Hmm split fiction", "start": 348, "duration": 3, "pause": 0},
             
-            {"text": "I might not express it much", "start": 334, "duration": 4, "pause": 0},
-            {"text": "You are very important to me", "start": 340, "duration": 5, "pause": 0},
-            {"text": "Hope you know that", "start": 347, "duration": 4, "pause": 0},
-            {"text": "Thank you for being here", "start": 353, "duration": 5, "pause": 0},
-            {"text": "As will i be", "start": 360, "duration": 4, "pause": 0},
+            {"text": "I might not express it much", "start": 353, "duration": 4, "pause": 0},
+            {"text": "You are very important to me", "start": 359, "duration": 5, "pause": 0},
+            {"text": "Hope you know that", "start": 366, "duration": 4, "pause": 0},
+            {"text": "Thank you for being here", "start": 372, "duration": 5, "pause": 0},
+            {"text": "As will i be", "start": 379, "duration": 4, "pause": 0},
             
-            {"text": "Got too sweet shit", "start": 366, "duration": 3, "pause": 0},
-            {"text": "Lmao tu end tak hai", "start": 371, "duration": 3, "pause": 0},
-            {"text": "Respect", "start": 376, "duration": 3, "pause": 0},
-            {"text": "Double sided tape bhi dede\nabhi yaad aaya ;-;", "start": 381, "duration": 4, "pause": 0},
+            {"text": "Got too sweet shit", "start": 385, "duration": 3, "pause": 0},
+            {"text": "Lmao tu end tak hai", "start": 390, "duration": 3, "pause": 0},
+            {"text": "Respect", "start": 395, "duration": 3, "pause": 0},
+            {"text": "Double sided tape bhi dede\nabhi yaad aaya ;-;", "start": 400, "duration": 4, "pause": 0},
             
-            {"text": "Hope you liked it", "start": 387, "duration": 4, "pause": 0},
-            {"text": "Chal ab band karde", "start": 393, "duration": 4, "pause": 0},
+            {"text": "Hope you liked it", "start": 406, "duration": 4, "pause": 0},
+            {"text": "Chal ab band karde", "start": 412, "duration": 4, "pause": 0},
         ]
         return messages
     
@@ -200,7 +200,7 @@ class MessageRenderer:
             # Position at back wall center
             # Calculate vertical offset for multi-line centering
             # Negative offset because y increases upward in 3D space
-            line_y = base_y - (i - len(lines)/2 + 0.5) * 0.35
+            line_y = base_y - (i - len(lines)/2 + 0.5) * 0.45
             
             # Project center point
             center_3d = Point3D(0, line_y, 9.5)
